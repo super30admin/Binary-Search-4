8d31287084c598eda3b5b9fe24da6f5862603681
Mahitee Satasiya <mahitee.satasiya@sjsu.edu>
2019-08-15 19:45:47 -0700

    Added simple approach and TC, SComplexity

--- H_Index_2.java
+++ H_Index_2.java
@@ -1,9 +1,29 @@
 package binarySearch4;
 
+//According to the definition of h-index on Wikipedia: 
+//"A scientist has index h if h of his/her N papers have at least h citations each, 
+//and the other N − h papers have no more than h citations each."
 public class H_Index_2 {
 
 	public int hIndex(int[] citations) {
-        int len = citations.length;
+     
+		// TC: O(logn) where n is the length of citations array, SC : o(1)
+	    
+	     int low = 0;
+	        int len = citations.length;
+	        int high = citations.length -1;
+	        
+	        while(low <= high) {
+	            int mid = low + (high-low)/2;
+	            if(citations[mid] == len - mid) return citations[mid];
+	            
+	            else if(citations[mid] > len - mid) high  = mid - 1;
+	            else low = mid + 1;
+	            
+	        }
+	        return len - (high + 1);
+		/*
+		int len = citations.length;
         if(len == 0)
             return 0;
         int end = len - 1;
@@ -21,6 +41,8 @@ public class H_Index_2 {
             return len - mid;
         else
             return 0;
+            
+            */
     }
 	
 }
