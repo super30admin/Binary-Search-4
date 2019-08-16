e31cdc48d056a5542a94e6ed7df4907be99ee161
Mahima Gupta <mahimagupta1195@gmail.com>
2019-08-15 18:53:11 -0700

    comments

--- intersect.java
+++ intersect.java
@@ -1,3 +1,5 @@
+//leetcode:: yes;
+//time and space: O(n)
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
