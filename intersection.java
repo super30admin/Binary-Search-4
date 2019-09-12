250fd841cdb817ffd78dc65f86bb152940a8f13d
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2019-09-11 22:18:28 -0700

    Add files via upload

---
+++ intersection.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        List<Integer> ans = new ArrayList<Integer>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        
+        for(int i=0, j=0; i<nums1.length && j<nums2.length; ){
+            
+               
+                if(nums1[i] == nums2[j]){
+                    ans.add(nums1[i]);
+                    i++;
+                    j++;
+                }else if(nums1[i] < nums2[j]){
+                    i++;
+                }else{
+                    j++;
+                }
+               
+        }
+        
+        int[] l = new int[ans.size()];
+        int i=0;
+        for(Integer k: ans){
+            l[i++] = k;
+        }
+   
+
+     return l;
+    }
+}
+
