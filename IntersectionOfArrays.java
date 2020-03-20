5e6fbe6542c790305ffa763de8c5d4cb52906429
Preksha Pansheriya <56806702+ppansheriya@users.noreply.github.com>
2020-03-19 17:02:53 -0700

    Add files via upload

---
+++ IntersectionOfArrays.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList();
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] == nums2[j]){
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }else if (nums1[i] < nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        
+        int[] result = new int[list.size()];
+        int k = 0;
+        for(int num : list){
+            result[k++] = num;
+        }
+        return result;
+        
+    }
+}
