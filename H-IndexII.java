e814272c64c01dde7f5a3ad9aa24c044cedcd5f9
Arpit Sharma <arpit317@gmail.com>
2020-08-23 14:43:47 -0700

    Update H-IndexII.java

--- H-IndexII.java
+++ H-IndexII.java
@@ -1,3 +1,5 @@
+//TC: O(n)
+//SC: O(1)
 class Solution {
     public int hIndex(int[] citations) {
         if(citations.length == 0) return 0;
@@ -8,5 +10,29 @@ class Solution {
             }
         }
         return 0;
+    }
+}
+
+//TC: O(log(n))
+//SC: O(1)
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int low = 0; int high = n - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int diff = n - mid;
+            if(citations[mid] == diff) return diff;
+            
+            if(citations[mid] < diff){
+                low = mid + 1;
+            }
+            
+            else{
+                high = mid - 1;
+            }
+        }
+        return n - low;
     }
 }
