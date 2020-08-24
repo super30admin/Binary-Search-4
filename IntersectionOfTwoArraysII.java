bfd1624cdacfc3b661abc17fb012eacec4e1eaff
Arpit Sharma <arpit317@gmail.com>
2020-08-23 21:12:12 -0700

    Update IntersectionOfTwoArraysII.java

--- IntersectionOfTwoArraysII.java
+++ IntersectionOfTwoArraysII.java
@@ -21,3 +21,31 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length) return intersect(nums2, nums1);
+        
+        int i = 0; int j = 0;
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        List<Integer> list = new ArrayList();
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++; j++;
+            }
+            else if(nums1[i] < nums2[j]) i++;
+            else j++;
+        }
+        
+        int[] res = new int[list.size()];
+        for(int k = 0; k < list.size(); k++){
+            res[k] = list.get(k);
+        }
+        return res;
+    }
+}
