23f5ae7863c339203f609fb2d4dada61cbb84e03
Arpit Sharma <arpit317@gmail.com>
2020-08-23 21:48:08 -0700

    Update IntersectionOfTwoArraysII.java

--- IntersectionOfTwoArraysII.java
+++ IntersectionOfTwoArraysII.java
@@ -1,3 +1,6 @@
+//TC: O(n+m)
+//SC: O(n), where n is the length of the smaller array
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -22,7 +25,8 @@ class Solution {
     }
 }
 
-
+//TC: O(n log(n)), if sorting is considered,  O(m+n), if arrays are already sorted
+//SC: O(1)
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         if(nums1.length > nums2.length) return intersect(nums2, nums1);
@@ -47,5 +51,55 @@ class Solution {
             res[k] = list.get(k);
         }
         return res;
+    }
+}
+
+//TC: n * O(log(m)), if arrays are already sorted, n is the length of smaller array
+//SC: O(1)
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length) return intersect(nums2, nums1);
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        List<Integer> list = new ArrayList();
+        int index = 0;
+        for(int i = 0; i < nums1.length; i++){
+            int bsIndex = binarySearch(nums2, index, nums2.length-1, nums1[i]);
+            if(bsIndex != -1){
+                list.add(nums1[i]);
+                index = bsIndex + 1;
+            }
+        }
+        
+        int[] res = new int[list.size()];
+        for(int k = 0; k < list.size(); k++){
+            res[k] = list.get(k);
+        }
+        return res;
+    }
+    
+    private int binarySearch(int[] arr, int low, int high, int num){
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            
+            if(arr[mid] == num) {
+                if(mid == low || arr[mid-1] < arr[mid]){
+                    return mid;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+            else if(arr[mid] > num){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
 }
