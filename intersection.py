11eae03e3f5e250d609c66e0fbe4a8e5c24aab97
atolat <arpantolat30@gmail.com>
2020-11-27 06:25:04 -0800

    Binary Search 4 - Solved 2 problems

--- intersection.py
+++ intersection.py
@@ -1,3 +1,7 @@
+# Time Complexity: O(m+n)
+# Space Complexity: O(min(m,n))
+# Approach: Store counts of larger array in hashmap. Traverse over the second array and check if elements are present in hashmap. If they are present, add them to a result array and decrement count in hashmap.
+from collections import Counter
 class Solution(object):
     def intersect(self, nums1, nums2):
         """
@@ -5,13 +9,11 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        intersect_map = {}
+        # Build a hashmap out of the longer array to save space
+        if len(nums1) > len(nums2):
+            self.intersect(nums2, nums1)
+        intersect_map = Counter(nums1)
         result = []
-        for x in nums1:
-            if x in intersect_map:
-                intersect_map[x] += 1
-            else:
-                intersect_map[x] = 1
         
         for y in nums2:
             if y in intersect_map and intersect_map[y] > 0:
--- median-of-arrays.py
+++ median-of-arrays.py
@@ -1,3 +1,6 @@
+# Time Comelexity: O(log(min(m,n)))
+# Space Complexity: In Place - O(1)
+# Approach: Try to find a partition such that every element in the left half of the partition is less than every element on the right half for both the arrays.
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -18,23 +21,33 @@ class Solution(object):
         partition_Y = 0
         
         while start_X <= end_X:
+            # partition_X divides the array nums1 in half
             partition_X = (start_X + end_X) / 2
+            
+            # partition_2 divides the array nums2 such that the combined number of elements in nums1:partition_x + nums2:partition_y is equal to the number of elements partition_x:end(nums1) + partition_y:end(nums2)
             partition_Y = ((x + y + 1) / 2) - partition_X
             
+            # Max number on left side of partition - nums1
             maxLeftX = nums1[partition_X - 1] if partition_X != 0 else float('-inf')
+            # Min number on right side of partition - nums1
             minRightX = nums1[partition_X] if partition_X != x else float('inf')
             
+            # Max number on left side of partition - nums2
             maxLeftY = nums2[partition_Y - 1] if partition_Y != 0 else float('-inf')
+            # Min number on right side of partition - nums2
             minRightY = nums2[partition_Y] if partition_Y != y else float('inf')
             
-            # Partition Found
+            # If this condition is true, partition is found
             if maxLeftX <= minRightY and minRightX >= maxLeftY:
+                # Even number of elements - take average of mid elements
                 if (x + y) % 2 == 0:
                     return (max(maxLeftX,maxLeftY) + min(minRightX,minRightY)) / 2.0
+                # Odd number of elements - max of left partition elements
                 else:
                     return max(maxLeftX,maxLeftY)
+            # Move search to left
             elif maxLeftX > minRightY:
                 end_X = partition_X - 1
-            else:
+            else: # Move search to right
                 start_X = partition_X + 1
                 
