080e7b8a6144ca703c60593a95a40bbc10333a9a
Mannat Dev <dev.m@husky.neu.edu>
2021-04-05 20:26:06 -0400

    Completed

--- PRoblem-1.java
+++ PRoblem-1.java
@@ -1,20 +1,55 @@
-//Time Complexity : O(logn)
-//Space Complexity : O(1)
+//Time Complexity :O(log Min(m,n))
+//Space Complexity :O(1)
 
 class Solution {
-    public int hIndex(int[] citations) {
-        if(citations == null || citations.length == 0) return 0;
-        
-        int low = 0;
-        int high = citations.length-1;
-        int n = citations.length;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            int trialhindex = n-mid;
-            if(citations[mid] == trialhindex) return trialhindex;
-            else if(citations[mid] < trialhindex) low = mid+1;
-            else high = mid-1;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length){
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
-        return n-low;
+        int low = 0;
+        int high = nums1.length;
+        int combinedLength = nums1.length + nums2.length;
+        
+        while(low<=high){
+            int partX = (low+high)/2;
+            int partY = (combinedLength +1)/2 - partX;
+            
+            int leftX = getMax(nums1, partX);
+            int rightX = getMin(nums1, partX);
+            int leftY = getMax(nums2, partY);
+            int rightY = getMin(nums2, partY);
+            
+            if(leftX <= rightY && leftY <=rightX){
+                if(combinedLength % 2 == 0){
+                    return (Math.max(leftX,leftY) + Math.min(rightX,rightY))/2.0;
+                }
+                            return Math.max(leftX,leftY);
+            }
+            
+            if(leftX > rightY){
+                high = partX - 1;
+            }
+            else {
+                low = partX +1;
+            }                 
+        }
+        return -1;
+    }
+    
+    private int getMax(int[] nums, int partition){
+        if(partition == 0)
+            return (int)Double.NEGATIVE_INFINITY;
+        
+    else 
+        return nums[partition-1];
+    }
+    
+    private int getMin(int[] nums, int partition){
+        if(partition == nums.length)
+            return (int)Double.POSITIVE_INFINITY;
+        else
+            return nums[partition];
     }
 }
--- Problem-2.java
+++ Problem-2.java
@@ -1,51 +1,37 @@
+//Time Complexity :O(Max(n,m)log(Max(n,m)))
+//Space Complexity :O(1)
 
-//Time Complexity : Max(n,m)logMAx(n,m)
-//Space Complexity : O(1)
-public class Main {
-    public static void main(String[] args) {
-        int [][] A = {{1,1000},{2,2000},{3,3000}};
-        int [][] B = {{1,1000},{2,2000},{3,3000}};
-        int target = 4000;
-        List<int []> output = new ArrayList<>();
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
         
-        Arrays.sort(B, (a,b) -> a[1] - b[1]); //mlogm
-        int max = Integer.MIN_VALUE;
+        int p1=0;
+        int p2=0;
+        int n=nums1.length;
+        int m=nums2.length;
         
-        for(int i = 0; i< A.length; i++){
-            int curr = A[i][1];
-            int index = binarySearch(B, target-curr);
-            
-            if(index != -1) {
-                int sum = curr+B[index][1];
-                if(sum>=max){
-                if(sum > max){
-                    output = new ArrayList<>();
-                }
-                    
-                output.add(new int[]{A[i][0], B[index][0]});
-                } 
+        List<Integer> output = new ArrayList<>();
+        
+        while(p1<n && p2<m){
+            if(nums1[p1] == nums2[p2])
+            {
+                output.add(nums1[p1]);
+                p1++;
+                p2++;
             }
-            for(int[] el : output){
-                System.out.println(el[0] + "," + el[1]);
+            else if(nums1[p1] < nums2[p2])
+            {
+                p1++;
+            }
+            else{
+                p2++;
             }
         }
-        
-    }
-    
-    public static int binarySearch(int[][] B, int target){
-        int low = 0;
-        int high = B.length;
-        while(low<=high){
-          int mid = low + (high-low)/2;
-            if(B[mid][1] == target)
-                return mid;
-            else if(B[mid][1] < target)
-                low = mid+1;
-            else
-                high = mid-1;
-            
+        int[] result = new int[output.size()];
+        for(int x=0; x<output.size(); x++){
+            result[x] = output.get(x);
         }
-        
-        return high;
+        return result;
     }
 }
