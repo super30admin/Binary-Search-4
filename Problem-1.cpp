288f652e9fe8d49248485a145671d01fb803b2e5
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-07 12:42:16 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -1,7 +1,7 @@
 /*
 Time Complexity = O(n+m)
 Space Complexity = O(n)
-where n is the number of elements in array nums1 and m is the number of elements in the array nums2.
+where n1 is the number of elements in array nums1 and n2 is the number of elements in the array nums2.
 */
 class Solution {
 public:
@@ -38,7 +38,7 @@ Space Complexity = O(1)
 if array is already sorted.
 Time Complexity = O(n + m)
 Space Complexity = O(1)
-where n is the number of elements in array nums1 and m is the number of elements in the array nums2.
+where n1 is the number of elements in array nums1 and n2 is the number of elements in the array nums2.
 */
 class Solution {
 public:
@@ -60,6 +60,51 @@ public:
                 p1++;
             else
                 p2++;
+        }
+        return ans;
+    }
+};
+
+/*
+Time Complexity = O(nlogm)
+Space Complexity = O(1)
+where n1 is the number of elements in array nums1 and n2 is the number of elements in the array nums2.
+*/
+class Solution {
+public:
+    int binarysearch(vector<int> nums, int target, int low, int high)
+    {
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){
+                if(mid==low || nums[mid-1]<nums[mid])
+                    return mid;
+                high = mid-1;
+            }
+            if(nums[mid]<target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return -1;
+    }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n1>n2)
+            intersect(nums2, nums1);
+        int i;
+        vector<int> ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int low = 0;
+        int high = nums2.size()-1;
+        for(i=0;i<n1;i++){
+            int index = binarysearch(nums2, nums1[i],low ,high);
+            if(index!=-1){
+                ans.push_back(nums1[i]);
+                low = index+1;
+            }
         }
         return ans;
     }
