e282be4b8c4b992fcca9281d7e901e9fcc1f57b7
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-07-08 10:57:59 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -85,3 +85,79 @@ class Solution {
         return arr;
     }
 }
+//****INTERSECTION OF TWO ARRAY-2: bIN****
+//Time complexity:O(mlogn)
+//Space complexity:o(1) where m is the length of smaller array;
+//Leetcode runnable:Y;
+//Any doubts:N;
+
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) 
+    {
+    
+        if(nums1.length>nums2.length)
+        {
+            intersect(nums2, nums1);
+        }
+        //Hence we have made sure now our nums1 is of smaller length than nums2 is bigger length
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> res=new ArrayList<>();
+        //We are planning to perform b.s on bigger array and giving the target from the smaller array
+        int low=0;
+        int high=nums2.length-1;
+        for(int i=0;i<nums1.length;i++)
+        {
+            int bs= Binarysearch(nums2,low, high, nums1[i]);
+                
+                if(bs!=-1)
+                {
+                    res.add(nums1[i]);
+                    low=bs+1;
+                    
+                }
+            
+        }
+
+        
+        int[] arr=new int[res.size()];
+        for(int i=0;i<res.size();i++)
+        {
+            arr[i]=res.get(i);
+        }
+        
+        return arr;
+    }
+    
+    private int Binarysearch(int[] nums2, int low, int high, int target)
+    {
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            
+            if(nums2[mid]==target)
+            {
+                if(mid==low || nums2[mid]!=nums2[mid-1])
+                {
+                    return mid;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+            else if(nums2[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return -1;
+        
+    }
+}
