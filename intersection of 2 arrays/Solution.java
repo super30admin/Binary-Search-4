edbbc66f2aef4c094e97f4b3e3b978d25e4fd352
Vraj Himanshu Reshamdalal <30725458+vraj9845@users.noreply.github.com>
2022-07-12 11:02:09 -0400

    Update Solution.java

--- intersection of 2 arrays/Solution.java
+++ intersection of 2 arrays/Solution.java
@@ -1,26 +1,25 @@
-//Time complexity: O(n)
-    // Space Complexity: O(n)
+// Time Complexity: O(n) ; n=length of bigger array
+// Space Complexity: O(m) ; m=length of smaller array
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> res=new ArrayList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        if(nums1.length>nums2.length) return intersect(nums2,nums1);
+        List<Integer> ls = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums1.length;i++){
-            if(map.containsKey(nums1[i])){
-                map.put(nums1[i], map.get(nums1[i])+1);
-            } else{
-                map.put(nums1[i],1);
-            }
+            map.put(nums1[i],map.getOrDefault(nums1[i],0) + 1);
         }
         for(int j=0;j<nums2.length;j++){
             if(map.containsKey(nums2[j])){
-                res.add(nums2[j]);
-                map.remove(nums2[j]);
+                map.put(nums2[j],map.get(nums2[j]) - 1);
+                map.remove(nums2[j],0);
+                ls.add(nums2[j]);
             }
         }
-        int []result = new int[res.size()];
-       for(int j=0;j<res.size();j++){
-           result[j]=res.get(j);
-       }
-        return result;
+        int []res = new int[ls.size()];
+        for(int i=0;i<ls.size();i++){
+            res[i]=ls.get(i);
+        }
+        return res;
     }
 }
