5058bb9662fb5c6db2df37897a932732ad7d3f93
Shubham <shub01gupta@gmail.com>
2022-08-11 21:20:01 +0530

    Completed Binary-Search-4

---
+++ h-index-ii.java
@@ -0,0 +1,25 @@
+// TC : O(logn)
+// SC : O(1)
+ 
+// BINARY SEARCH SOLUTION
+
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0 ) return 0;
+        
+        int n = citations.length;
+        int low =0;
+        int high =n-1;  
+        
+        while(low<=high){
+            int mid = low+ (high-low)/2;
+            if(citations[mid] >= n-mid){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return n-low;
+    }
+}
