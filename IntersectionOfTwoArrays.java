ceb553db1a6498daa1aded2e645fff68998fce46
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-02 22:52:35 -0700

    Update IntersectionOfTwoArrays.java

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -44,7 +44,7 @@ class Solution {
 //Technique2 : Two Pointers
 //Time Complexity = O(max(m,n)) Traversing the first array  and Second Array, atleast we will traverse fully one Array, thats max(arr1,arr2)
 //Space Complexity = O(1) we are not using any extra space here
-//TC = O(m+n)
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
@@ -86,4 +86,60 @@ class Solution {
         return arrayResult;
     }
 
+}
+
+//Technique3 : Binary Search
+//Time Complexity = O(mlogn) Traversing the smallest array  and apply Binary Search on another Big array
+//Space Complexity = O(1) we are not using any extra space here
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        if (n2 > n1) return intersect(nums2, nums1);
+
+        List < Integer > result = new ArrayList <> ();
+
+        int low = 0;
+        
+        for (int i = 0; i < n1; i++) {
+
+            int high = n2 - 1;
+
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+                 
+                
+                if (nums2[mid] == nums1[i]) {
+
+
+                    if (low < mid && nums2[mid] == nums2[mid - 1]) {
+                        high = mid - 1;
+                    } else {
+                        result.add(nums2[mid]);
+                        low = mid + 1;
+                        break;
+                    }
+                } else if (nums2[mid] > nums1[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } // while
+
+        } //for
+
+
+        System.out.println(result);
+        int[] arrayResult = new int[result.size()];
+        for (int k = 0; k < result.size(); k++) {
+            arrayResult[k] = result.get(k);
+        }
+        return arrayResult;
+
+    }
 }
