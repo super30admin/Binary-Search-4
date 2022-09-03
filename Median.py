4ff0928b05b61166b06ff73a9171c36a51780c64
Hardik Jain <89613477+jhardik0022@users.noreply.github.com>
2022-09-03 12:17:11 -0700

    Completed Median.py

---
+++ Median.py
@@ -0,0 +1,42 @@
+# Time complexity : O(n) --> n = length of smaller array
+# Space complexity : O(1)
+# Leetcode : Solved and submitted
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # fetch the lengths of 2 arrays
+        n1 = len(nums1)
+        n2 = len(nums2)
+        
+        # make n1 as the smaller array
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        # partitioning the array from 0 till n1, so that in every partition, we have just one element
+        low = 0
+        high = n1
+        
+        # traverse until the low <= high
+        while low <= high:
+            # find the partition of array using the binary searcg
+            partX = low + (high - low) // 2
+            # find partition of Y using the median formula such that each side has equal elements if even and left has one more than right for odd
+            partY = (n1 + n2) // 2 - partX
+                        
+            # fetch l1, l2, r1 and r2 using the partition index of X and Y
+            l1 = float('-inf') if partX == 0 else nums1[partX - 1]
+            r1 = float('inf') if partX == n1 else nums1[partX]
+            l2 = float('-inf') if partY == 0 else nums2[partY - 1]
+            r2 = float('inf') if partY == n2 else nums2[partY]
+            
+            # check if the after partition, we have the array sorted, if so, then return the result as avg for even numbers and middle when odd
+            if l1 <= r2 and l2 <= r1:
+                if (n1 + n2)%2 == 0:
+                    return (max(l1,l2)+min(r1,r2)) / 2
+                else:
+                    return min(r1,r2)
+            
+            # if the l2 > r1, that means we need to shift the partition to the right side, else move to the left side
+            if l2 > r1:
+                low = partX + 1
+            else:
+                high = partX - 1
