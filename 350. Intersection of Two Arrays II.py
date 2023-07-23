45950d68427b2825683700d8e1ff4a9068c7392f
Nickil Vishwaa MS <nickilvishwaams@gmail.com>
2023-07-23 19:16:54 +0530

    Completed Binary-Search-4

---
+++ 350. Intersection of Two Arrays II.py
@@ -0,0 +1,25 @@
+#Time Complexity:O(log m(size of greater array))
+#Space Complexity:O(1)
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        lst=[]
+        nums1.sort()
+        nums2.sort()
+        i=0
+        j=0
+        while i< len(nums1) and j< len(nums2) :
+            if nums1[i]==nums2[j] :
+                lst.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i] > nums2[j] :
+                j+=1
+            elif nums1[i] < nums2[j] :
+                i+=1
+        return lst
