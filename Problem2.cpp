cd02cb00685537dc6981e4a5f61ff77569abe2a7
pallavithorat <thoratpallavi2000@gmail.com>
2024-02-11 23:09:02 -0800

    Binary-Search-4 completed

---
+++ Problem2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;
+        int n = n1 + n2;
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : INT_MIN;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX;
+            int r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return max(l1, l2);
+                return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
+        }
+
+        return 0;
+    }
+    
+};
